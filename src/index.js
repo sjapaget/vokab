98675972ff6fd73e44ebfd3907eb616e081f05e9
Samuel Paget <sjapaget@gmail.com>
2024-10-27 18:31:30 +0100

    move settings js to its own file

--- src/index.js
+++ src/index.js
@@ -1,14 +1,1 @@
-import domReady from '@wordpress/dom-ready';
-import { createRoot } from '@wordpress/element';
-
-const SettingsPage = () => {
-    return <div>Placeholder for settings page</div>;
-};
-
-domReady( () => {
-    const root = createRoot(
-        document.getElementById( 'vokab-settings' )
-    );
-
-    root.render( <SettingsPage /> );
-} );
+import './settings';
